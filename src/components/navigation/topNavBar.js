46e0a6e460f6f82bb90de677d1a799c69cebcb75
bobbyaxe61 <bobbyaxe61@gmail.com>
2020-08-13 12:31:25 +0100

    Styled Navbar Logo For Small Screen

--- src/components/navigation/topNavBar.js
+++ src/components/navigation/topNavBar.js
@@ -4,11 +4,15 @@ export class TopNavBar extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top liqair-border-image-top">
+            <div className="container-fluid liqair-border-image-top">
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top pt-3">
+                <a className="navbar-brand d-block d-md-none" href="/">
+                    <img src={process.env.REACT_APP_PUBLIC_URL+'/assets/img/icons/liqair.png'} width={120} height={50} className="img-fluid" alt="logo" loading="lazy" />
+                </a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
-                <div className="collapse navbar-collapse">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
                     <ul className="navbar-nav mt-2 mt-lg-0">
                         <li className="nav-item">
                             <a href="#home" className="nav-link mx-3">Home</a>
@@ -17,7 +21,7 @@ export class TopNavBar extends Component {
                             <a href="#works" className="nav-link mx-3">Works</a>
                         </li>
                         <li className="nav-item">
-                            <a href="/" className="nav-item px-3 mx-5">
+                            <a href="/" className="d-none d-md-block px-3 mx-5">
                                 <img src={process.env.REACT_APP_PUBLIC_URL+'/assets/img/icons/liqair.png'} width={120} height={50} className="img-fluid" alt="logo" loading="lazy" />
                             </a>
                         </li>
@@ -30,6 +34,7 @@ export class TopNavBar extends Component {
                     </ul>
                 </div>
             </nav>
+            </div>
         )
     }
 }
