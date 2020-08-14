99263ab2e0ffff6c93d232998136a29f8958fe31
bobbyaxe61 <bobbyaxe61@gmail.com>
2020-08-14 03:33:07 +0100

    Style Modifications To TopNavBar

--- src/components/navigation/topNavBar.js
+++ src/components/navigation/topNavBar.js
@@ -4,8 +4,7 @@ export class TopNavBar extends Component {
 
     render() {
         return (
-            <div className="container-fluid liqair-border-image-top">
-            <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top pt-3">
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
                 <a className="navbar-brand d-block d-md-none" href="/">
                     <img src={process.env.REACT_APP_PUBLIC_URL+'/assets/img/icons/liqair.png'} width={120} height={50} className="img-fluid" alt="logo" loading="lazy" />
                 </a>
@@ -15,10 +14,10 @@ export class TopNavBar extends Component {
                 <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
                     <ul className="navbar-nav mt-2 mt-lg-0">
                         <li className="nav-item">
-                            <a href="#home" className="nav-link mx-3">Home</a>
+                            <a href="#home" className="nav-link mx-3"><span className="liqair-color">Home</span></a>
                         </li>
                         <li className="nav-item">
-                            <a href="#works" className="nav-link mx-3">Works</a>
+                            <a href="#works" className="nav-link mx-3"><span className="liqair-color">Works</span></a>
                         </li>
                         <li className="nav-item">
                             <a href="/" className="d-none d-md-block px-3 mx-5">
@@ -26,15 +25,14 @@ export class TopNavBar extends Component {
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a href="#services" className="nav-link mx-3">Services</a>
+                            <a href="#services" className="nav-link mx-3"><span className="liqair-color">Services</span></a>
                         </li>
                         <li className="nav-item">
-                            <a href="#clients" className="nav-link mx-3">Clients</a>
+                            <a href="#clients" className="nav-link mx-3"><span className="liqair-color">Clients</span></a>
                         </li>
                     </ul>
                 </div>
             </nav>
-            </div>
         )
     }
 }
